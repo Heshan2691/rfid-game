568ad968981f1d46d3a65b13730e8e6aaf123290
Heshan2691 <131267985+Heshan2691@users.noreply.github.com>
2024-06-19 19:19:20 +0530

    try again styles

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Start from './components/start';
 import Round2 from './components/round2';
 import R1t1 from './components/r1t1'
+import Tryagain from './components/tryagain';
 
 
 function App() {
   return (
     <div className="App">
-        <R1t1 />
+        <Tryagain />
 
         
 
