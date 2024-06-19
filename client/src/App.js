895995b70ea65905a9345b90181adc9a895a06e6
Sulakshani Dissanayake <sulakshanidissanayake@gmail.com>
2024-06-19 23:16:01 +0530

    last

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,11 @@
 import './App.css';
-import Countdown from './components/Countdown';
-import Guideline1 from './components/Guideline1';
+
 import Level1 from './components/Level1';
-import Round1 from './components/Round1';
-import Start from './components/start';
-<<<<<<< HEAD
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-=======
-import Round2 from './components/round2';
-import R1t1 from './components/r1t1'
-import Tryagain from './components/tryagain';
->>>>>>> 568ad968981f1d46d3a65b13730e8e6aaf123290
-
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-        <Countdown/>
-=======
-        <Tryagain />
-
-        
-
->>>>>>> 568ad968981f1d46d3a65b13730e8e6aaf123290
+        <Level1 />
     </div>
   );
 }
